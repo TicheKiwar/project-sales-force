124ed21fb267891681e79fc9c2292da1bc8d58a5
TicheKiwar <kiwartiche@gmail.com>
2024-05-21 14:19:39 -0500

    Create entitys and module user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConnectionModule } from './interfaces/controllers/controller';
+import { UserModule } from './modules/user/user.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [ConnectionModule],
+  imports: [ConnectionModule, UserModule],
   controllers: [],
   providers: [],
 })
---
+++ src/domain/entity/Persons.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Customers } from "./Customers";
+
+@Index("persons_pkey", ["personId"], { unique: true })
+@Entity("persons", { schema: "public" })
+export class Persons {
+  @PrimaryGeneratedColumn({ type: "integer", name: "person_id" })
+  personId: number;
+
+  @Column("character", { name: "cedula", length: 10 })
+  cedula: string;
+
+  @Column("character varying", { name: "first_name", length: 25 })
+  firstName: string;
+
+  @Column("character varying", {
+    name: "middle_name",
+    nullable: true,
+    length: 25,
+  })
+  middleName: string | null;
+
+  @Column("character varying", { name: "last_name", length: 25 })
+  lastName: string;
+
+  @Column("date", { name: "birth_date" })
+  birthDate: string;
+
+  @OneToMany(() => Customers, (customers) => customers.person)
+  customers: Customers[];
+}
